e0de83fe2190820f5ce69b76dd8f1a22514d9f1a
Muaz <169424072+Raycasting9@users.noreply.github.com>
2025-03-01 14:48:31 +0800

    Add feature to change grid size

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
-
+//Variables
 const gridBox = document.querySelector('.grid');
-
-for (let i = 0; i<16*16; i++) {
-    const div = document.createElement('div');
-    div.classList.add('grid-item');
-    gridBox.appendChild(div);
-}
-
+const defaultgrid = 16;
 const gridItems = document.querySelectorAll('.grid-item');
 
-gridItems.forEach(item => {
-    item.addEventListener("mouseover", () => {
-        item.style.backgroundColor = "black";
-    });
+//Process
+createGrid(defaultgrid);
+
+//Hover Event
+
+gridBox.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("grid-item")) {
+        event.target.style.backgroundColor = "black";
+    }
 });
 
-function changeSize() {
-    while (newSize >= 100) {
-        let newSize = prompt("Enter new grid size [max: 100]");
+
+//Changing size for each grid [Fuction]
+function newSize() {
+
+    let newSize = prompt("Enter new grid size [max: 100]");
+
+    if (newSize >= 100){
+        alert("More than 100");
+        return;
     }
     
-    
+    gridBox.innerHTML="";
 
+    createGrid(newSize);
+    changeFlex(newSize);
+
+}
+
+//Creating grids [Function]
+function createGrid(value) {
+
+    for (let i = 0; i<value*value; i++) {
+        const div = document.createElement('div');
+        div.classList.add('grid-item');
+        gridBox.appendChild(div);
+    }
+
+    changeFlex(value);
+}
+
+//Change property [Fucntion]
+function changeFlex(value){
+
+    let newFlex = `1 1 calc(100% / ${value}`;
+    document.querySelectorAll(".grid-item").forEach(item => {
+        item.style.flex = newFlex;
+    });
 }
